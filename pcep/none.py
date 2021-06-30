f5ac34c5712bbfd006fe69505c82d7925738bef3
Tim <swollen.and.halo@gmail.com>
2021-06-30 21:31:30 +0300

    functions

---
+++ pcep/none.py
@@ -0,0 +1,19 @@
+#!/data/data/com.termux/files/usr/bin/python3
+
+def my_func(n):
+    if n % 2 == 0:
+        return True
+
+print(my_func(4))
+print(my_func(5))
+
+def nu_func(m):
+    if m % 2 == 0:
+        return True
+    else:
+        return False
+
+print(nu_func(4))
+print(nu_func(5))
+
+
