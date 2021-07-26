5c418bff46978b54979e3132cec8c1b1eb6d65fd
Tim <swollen.and.halo@gmail.com>
2021-07-26 11:50:12 +0300

    pcap

--- projects/countries.py
+++ projects/countries.py
@@ -1,3 +1,5 @@
+#!/data/data/com.termux/files/usr/bin/python3
+
 countries = ['united states', 'latvia', 'germany', 'finland', 'poland', 'czechia', 'turkey', 'egypt', 'ukraine', 'russia']
 
 print(sorted(countries))
